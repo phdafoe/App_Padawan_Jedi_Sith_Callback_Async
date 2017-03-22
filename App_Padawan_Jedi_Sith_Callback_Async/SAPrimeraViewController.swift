c23abaa3485d7692c64d9626f79380cea4687b85
phdafoe <formador@aula8PovMacNfor.local>
2017-03-22 19:25:27 +0100

    Ajustes_2

---
+++ App_Padawan_Jedi_Sith_Callback_Async/SAPrimeraViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SAPrimeraViewController.swift
+//  App_Padawan_Jedi_Sith_Callback_Async
+//
+//  Created by formador on 22/3/17.
+//  Copyright © 2017 formador. All rights reserved.
+//
+
+import UIKit
+
+class SAPrimeraViewController: UIViewController {
+    
+    //MARK: - Variables locales
+    
+    
+    
+    //MARK: - IBoutlets
+    @IBOutlet weak var myImageViewCustom: UIImageView!
+    @IBOutlet weak var myWebViewCustom: UIWebView!
+    
+    
+    
+    //MARK: - IBActions
+    @IBAction func showDataFromUrlACTION(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
